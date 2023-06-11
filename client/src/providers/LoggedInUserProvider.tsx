08b2bcc96f42c3e9c3e5b8287a8d038d34ecd92a
Vytautas Šerelis <vytutas2@gmail.com>
2023-06-11 17:06:50 +0300

    tweak(client): check for current session

--- client/src/providers/LoggedInUserProvider.tsx
+++ client/src/providers/LoggedInUserProvider.tsx
@@ -1,9 +1,23 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { ILoggedInUser } from "../typings/user";
 import { LoggedInUserCtx } from "../context/LoggedInUserCtx";
+import { fetchApi } from "../utils/fetch";
 
 const LoggedInUserProvider = ({ children }: { children: React.ReactNode }) => {
   const [loggedInUser, setLoggedInUser] = useState<ILoggedInUser>();
+
+  useEffect(() => {
+    return () => {
+      fetchApi({
+        path: "/api/users/getAuthUser",
+        method: "GET",
+      }).then(({ data }: { data: { user: ILoggedInUser } }) => {
+        if (data) {
+          setLoggedInUser(data.user);
+        }
+      });
+    };
+  }, []);
 
   return (
     <LoggedInUserCtx.Provider value={{ loggedInUser, setLoggedInUser }}>
--- server/src/routes/users.ts
+++ server/src/routes/users.ts
@@ -6,5 +6,6 @@ const router = express.Router();
 router.get('/', UsersController.getAuthUser);
 router.post('/signup', UsersController.signUp);
 router.post('/login', UsersController.login);
+router.get('/getAuthUser', UsersController.getAuthUser);
 
 export default router;
